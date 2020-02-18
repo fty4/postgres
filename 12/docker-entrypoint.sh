0ecc6f699b052cc39f5521794c6fa28cbe18e747
Marco <github@marco.lecheler.com>
2020-02-18 10:47:25 +0100

    change chmod to allow group rwx as well

--- 12/docker-entrypoint.sh
+++ 12/docker-entrypoint.sh
@@ -37,7 +37,7 @@ docker_create_db_directories() {
 	local user; user="$(id -u)"
 
 	mkdir -p "$PGDATA"
-	chmod 700 "$PGDATA"
+	chmod 770 "$PGDATA"
 
 	# ignore failure since it will be fine when using the image provided directory; see also https://github.com/docker-library/postgres/pull/289
 	mkdir -p /var/run/postgresql || :
@@ -49,7 +49,7 @@ docker_create_db_directories() {
 		if [ "$user" = '0' ]; then
 			find "$POSTGRES_INITDB_WALDIR" \! -user postgres -exec chown postgres '{}' +
 		fi
-		chmod 700 "$POSTGRES_INITDB_WALDIR"
+		chmod 770 "$POSTGRES_INITDB_WALDIR"
 	fi
 
 	# allow the container to be started with `--user`
